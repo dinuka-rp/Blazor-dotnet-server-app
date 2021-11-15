f3b5e3f20230d98fbc71f3beee4bc437c573ec9e
dinuka-rp <drpiyadigama@gmail.com>
2021-11-15 12:17:55 +0530

    fix TicketService injection in Startup.cs, create a separate page for project-board

--- ProjectManagementSystem/Startup.cs
+++ ProjectManagementSystem/Startup.cs
@@ -41,6 +41,7 @@ namespace ProjectManagementSystem
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<TicketService>();     // TODO: check if this should be Singleton?
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

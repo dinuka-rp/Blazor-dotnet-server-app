e2bd943daee37f758bdbfada546e2d9d8c34bec8
dinuka-rp <drpiyadigama@gmail.com>
2021-11-28 16:23:50 +0530

    create all basic CRUD services for entities

--- ProjectManagementSystem/Models/CompanyService.cs
+++ ProjectManagementSystem/Models/CompanyService.cs
@@ -21,11 +21,47 @@ namespace ProjectManagementSystem.Models
         }
         #endregion
 
-        #region Get List of Users
+        #region Get List of Companies
         public async Task<List<Company>> GetAllCompaniesAsync()
         {
             return await _applicationDbContext.Companies.ToListAsync();
         }
         #endregion
+
+        #region Insert Company
+        public async Task<bool> InsertCompanyAsync(Company company)
+        {
+            await _applicationDbContext.Companies.AddAsync(company);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Get Company by Id
+        public async Task<Company> GetCompanyAsync(Guid Id)
+        {
+            Company company = await _applicationDbContext.Companies.FirstOrDefaultAsync(c => c.Id.Equals(Id));
+            return company;
+        }
+        #endregion
+
+        #region Update Company
+        public async Task<bool> UpdateCompanyAsync(Company company)
+        {
+            _applicationDbContext.Companies.Update(company);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Delete Company
+        public async Task<bool> DeleteCompanyAsync(Company company)
+        {
+            _applicationDbContext.Remove(company);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
     }
 }
--- ProjectManagementSystem/Models/UserService.cs
+++ ProjectManagementSystem/Models/UserService.cs
@@ -28,11 +28,47 @@ namespace ProjectManagementSystem.Models
         #endregion
 
         #region Get List of Users - with pagination
-        //public async Task<List<Ticket>> GetAllTicketsWithPaginationAsync()
+        //public async Task<List<ApplicationUser>> GetAllUsersWithPaginationAsync()
         //{
         //    // TODO: add pagination
-        //    return await _applicationDbContext.Tickets.ToListAsync();
+        //    return await _applicationDbContext.Users.ToListAsync();
         //}
         #endregion
+
+        #region Insert User
+        public async Task<bool> InsertUserAsync(ApplicationUser user)
+        {
+            await _applicationDbContext.Users.AddAsync(user);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Get Ticket by Id
+        public async Task<ApplicationUser> GetUserAsync(String Id)
+        {
+            ApplicationUser user = await _applicationDbContext.Users.FirstOrDefaultAsync(c => c.Id.Equals(Id));
+            return user;
+        }
+        #endregion
+
+        #region Update Ticket
+        public async Task<bool> UpdateUserAsync(ApplicationUser user)
+        {
+            _applicationDbContext.Users.Update(user);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Delete Ticket
+        public async Task<bool> DeleteUserAsync(ApplicationUser user)
+        {
+            _applicationDbContext.Remove(user);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
     }
 }

a81ab729683d13d73201e413358ffc1baac650b3
dinuka-rp <drpiyadigama@gmail.com>
2022-01-03 15:11:18 +0530

    implement logic for prediction service

--- ProjectManagementSystem/Models/PredictionService.cs
+++ ProjectManagementSystem/Models/PredictionService.cs
@@ -21,16 +21,76 @@ namespace ProjectManagementSystem.Models
         #endregion
 
         #region Get Prediction
-        //public async Task<PredictionDTO> GeneratePrediction(DateTime date)
-        //{
-        //    //use LINQ - to get required details here
-        //    //Int32 projectsPerCompany = await _applicationDbContext.
+        public async Task<PredictionDTO> GeneratePrediction(DateTime predictionDate)
+        {
+            Int32 dateDiff = (Int32)(predictionDate - DateTime.Now).TotalDays;
+            DateTime pastDate = predictionDate.Subtract(predictionDate - DateTime.Now);
 
-        //    //return new PredictionDTO { 
-        //    //    PeoplePerProject=,
-        //    //    ProjectsPerCompany=,
-        //    //};
-        //}
+
+            // use LINQ - to get required details here
+            // TODO: check if the logic to get the projects is correct here
+            List<Project> projectsInCurrentMonth = await _applicationDbContext.Projects.Where(project => project.CompletedOn == null &&
+                                                DateTime.Now > project.StartedOn).ToListAsync();
+            List<Project> projectsInPastNMonth = await _applicationDbContext.Projects.Where(project => pastDate < project.StartedOn &&
+                                                (pastDate < project.CompletedOn || project.CompletedOn == null)).ToListAsync();
+
+            // ---------------
+            HashSet<Company> companiesInCurrentMonth = new();
+            foreach (var project in projectsInCurrentMonth)
+            {
+                companiesInCurrentMonth.Add(project.Company);
+            }
+
+            HashSet<Company> companiesInNMonth = new();
+            foreach (var project in projectsInPastNMonth)
+            {
+                companiesInNMonth.Add(project.Company);
+            }
+
+            // Projects per company prediction
+            Int32 projectsPerCompanyCurrent = projectsInCurrentMonth.Count / companiesInCurrentMonth.Count;
+            Int32 projectsPerCompanyNMonth = projectsInPastNMonth.Count / companiesInNMonth.Count;
+
+
+            Int32 expectedIncreaseInProjectsPerCompany = (projectsPerCompanyCurrent - projectsPerCompanyNMonth) / dateDiff;
+
+            // prediction calculation
+            Int32 projectsPerCompanyPrediction = projectsPerCompanyCurrent * expectedIncreaseInProjectsPerCompany;
+
+            // ---------------
+            HashSet<ApplicationUser> usersInCurrentMonth = new();
+            foreach (var project in projectsInCurrentMonth)
+            {
+                foreach (var user in project.Users)
+                {
+                    usersInCurrentMonth.Add(user);
+                }
+            }
+
+            HashSet<ApplicationUser> usersInPastNMonth = new();
+            foreach (var project in projectsInCurrentMonth)
+            {
+                foreach (var user in project.Users)
+                {
+                    usersInPastNMonth.Add(user);
+                }
+            }
+
+            // Users per project prediction
+            Int32 usersPerProjectCurrent = usersInCurrentMonth.Count / projectsInCurrentMonth.Count;
+            Int32 usersPerProjectNMonth = usersInPastNMonth.Count / projectsInPastNMonth.Count;
+
+            Int32 expectedIncreaseInUsersPerProject = (usersPerProjectCurrent - usersPerProjectNMonth) / dateDiff;
+
+            // prediction calculation
+            Int32 peoplePerProjectPrediction = usersPerProjectCurrent * expectedIncreaseInUsersPerProject;
+
+            return new PredictionDTO
+            {
+                PeoplePerProject = peoplePerProjectPrediction,
+                ProjectsPerCompany = projectsPerCompanyPrediction,
+            };
+        }
         #endregion
     }
 }
